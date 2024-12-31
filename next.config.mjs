8d3761511ab3e56a70c2f13a29b9a53fefc36927
Ahmed Elshazly <57854391+Shazly99@users.noreply.github.com>
2024-12-31 14:11:25 +0200

    s

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,32 @@ const nextConfig = {
     })
     return config
   },
+  "plugins": [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        "autoprefixer": {
+          "flexbox": "no-2009"
+        },
+        "stage": 3,
+        "features": {
+          "custom-properties": false
+        }
+      }
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ["html", "body"]
+      }
+    ],
+  ]
 };
 
 export default withNextIntl(nextConfig);
